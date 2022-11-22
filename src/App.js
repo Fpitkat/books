f11f43b784fe0f57702dace14f8c553346b97ecc
Fpitkat <75321987+Fpitkat@users.noreply.github.com>
2022-11-22 11:35:48 -0500

    BookShow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { useState } from 'react'
 import BookCreate from './components/BookCreate'
+import BookList from './components/BookList'
 
 function App() {
   const [books, setBooks] = useState([])
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id
+    })
+
+    setBooks(updatedBooks)
+  }
 
   const createBook = (title) => {
     const updatedBooks = [
@@ -17,10 +26,13 @@ function App() {
   }
 
   const editBook = () => {}
-  const deleteBook = () => {}
 
   return (
-    <div>
+    <div className='app'>
+      <BookList
+        books={books}
+        onDelete={deleteBookById}
+      />
       <BookCreate onCreate={createBook} />
     </div>
   )
